58da5110b5721693d0e95c8a15ccb89dd8ac5cdb
Aiden Wei <@>
2025-02-08 14:51:13 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,15 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int output = 0;
+        while (x < 10) {
+            output += x;
+            System.out.print(output + " ");
+            x = x + 1;
+            if (x == 10) {
+                System.out.println();
+            }
+        }
+    }
+}
+
